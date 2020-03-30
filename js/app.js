987724316a7a471ba670f6ff7903d4f66ae19e11
Gaston Gonzalez Sapia <ggonzalezsapia@gmail.com>
2020-03-30 09:57:46 -0300

    Modificamos la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 
+var url = window.location.href;
+var swLocation = '/CursoPWA_Twittor/sw.js';
+
 if (navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+  if(url.includes('localHost')){
+      swLocation = '/sw.js';
+  }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
